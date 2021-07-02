6149a5e4b183a2d09e117e5f137e29559b9faef2
amoh-godwin <amohgyebigodwin@gmail.com>
2021-07-02 11:01:26 +0000

    transfer gracously

---
+++ main.py
@@ -0,0 +1,63 @@
+from typing import Optional, List
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from deta import Deta
+
+
+
+app = FastAPI()
+
+deta = Deta()
+db = deta.Base('simpleDB')  # access your DB
+drive = deta.Drive("images")
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+@app.post("/files/")
+def create_files(files: List[bytes] = File(...)):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+    for file in files:
+        name = file.filename
+        f = file.file
+        res = drive.put(name, f)
+
+    return {"result": res, "filenames": [file.filename for file in files]}
+
+@app.get('/')
+def read_root():
+    content = """
+<body>
+<h1>Hello</h1>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: Optional[str] = None):
+    return {'item_id': item_id, 'q': q}
+
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id}
+
+
