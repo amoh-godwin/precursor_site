c795c5f2ef44897e4ccbbe18069d68736c470fdb
amoh-godwin <amohgyebigodwin@gmail.com>
2021-07-04 13:06:26 +0000

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import os
 
 if not os.path.exists('./static'):
@@ -13,3 +14,10 @@ def read_pages(page: str):
         contents = p_o.read()
 
     return contents
+
+
+def save_image(name: str, fp: BytesIO):
+    with open(name, 'wb') as ff:
+        ff.write(fp.read())
+
+    return name
