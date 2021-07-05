ab5636a7dd646c166a66fc3d03c498f3ebb8efc3
amoh-godwin <amohgyebigodwin@gmail.com>
2021-07-05 00:44:19 +0000

    fix markdown

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI, UploadFile, File, Form
 from pydantic import BaseModel
 from deta import Deta, service
 
-from misc import read_pages
+from misc import read_pages, fix_md_images_link
 
 
 with open('key.txt', mode='r') as k_f:
@@ -50,6 +50,9 @@ def create_post(title: str = Form(...), headerfile: UploadFile = File(...), cont
         ff = file.file
         res = drive.put(name, ff)
         result.append(res)
+
+    # Fix the markdown
+    content = fix_md_images_link(content)
 
     # save to db
     res = art_db.put({
